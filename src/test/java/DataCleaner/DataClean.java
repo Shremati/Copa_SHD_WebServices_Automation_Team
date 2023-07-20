56db414a77f76af7b289537da6927f954028d466
Sauvik Sarkar <sauvik.sarkar@dxc.com>
2023-07-20 12:41:21 +0530

    First Commit to Reflect in Origin/Sauvik

--- src/test/java/DataCleaner/DataClean.java
+++ src/test/java/DataCleaner/DataClean.java
@@ -11,11 +11,12 @@ public class DataClean
     public static void main(String[] args)
     {
 
-       // Delete the files and sub-folders of a directory - use below methods only before checkin to github
+       // Delete the files and sub-folders of a directory - use below methods only before checkin to GitHub
         deleteFolder(new File(getResponseDirectory()));
         deleteFolder(new File(getAllureReports()));
         deleteFolder(new File(getAllureResults()));
         deleteFolder(new File(getTarget()));
+
     }
 
 }
