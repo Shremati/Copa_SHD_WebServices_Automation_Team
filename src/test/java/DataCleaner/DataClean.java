b2c63f03da521e6d9dc344ee55a531c144e9d08f
Sauvik Sarkar <sauvik.sarkar@dxc.com>
2023-08-25 22:14:29 +0530

    Modified ManageSessions and ModifyTicketingService

--- src/test/java/DataCleaner/DataClean.java
+++ src/test/java/DataCleaner/DataClean.java
@@ -11,7 +11,7 @@ public class DataClean
     public static void main(String[] args)
     {
 
-       // Delete the files and sub-folders of a directory - use below methods only before checkin to GitHub
+       // Delete  the files and sub-folders of a directory - use below methods only before checkin to GitHub
         deleteFolder(new File(getResponseDirectory()));
         deleteFolder(new File(getAllureReports()));
         deleteFolder(new File(getAllureResults()));
