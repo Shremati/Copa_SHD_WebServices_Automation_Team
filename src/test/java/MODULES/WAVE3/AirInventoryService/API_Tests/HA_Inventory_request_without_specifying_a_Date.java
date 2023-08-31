cec5c125853496a81c12422a1b7860c18706d984
Sauvik Sarkar <sauvik.sarkar@dxc.com>
2023-08-31 18:12:48 +0530

    Added New Scenarios from Rohin

---
+++ src/test/java/MODULES/WAVE3/AirInventoryService/API_Tests/HA_Inventory_request_without_specifying_a_Date.java
@@ -0,0 +1,92 @@
+package MODULES.WAVE3.AirInventoryService.API_Tests;
+
+import GENERICS.Utils;
+import GENERICS.XMLParser;
+import frameworkconstants.FrameworkConstants;
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.response.Response;
+import org.apache.commons.io.IOUtils;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static io.restassured.RestAssured.given;
+
+public class HA_Inventory_request_without_specifying_a_Date extends FrameworkConstants
+{
+    public static String SOAPRequest;
+
+    public static void Execute() throws IOException, ParserConfigurationException, TransformerException, SAXException
+    {
+
+
+        UpdatePayload();
+
+//    ******** Read the updated request and send it to fetch the response *********
+
+        FileInputStream fileInputStream = new FileInputStream(getTemp_requestPath());
+        SOAPRequest= IOUtils.toString(fileInputStream, "UTF-8");
+        SOAPRequest = SOAPRequest.substring(SOAPRequest.indexOf('\n') + 1);
+
+        Response response = given()
+                .baseUri(getBaseURL())
+                .header("Content-Type", "text/xml")
+                .filter(new AllureRestAssured())
+                .body(SOAPRequest)
+                .when()
+                .post(getAirinventoryservice())
+                .then()
+                .statusCode(200)
+                .and()
+                .log().all().extract().response();
+
+
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter(getResponseDirectory()+"AirInventoryService\\HA_Inventory_request_without_specifying_a_Date.xml"));
+        writer.write(response.asPrettyString());
+        writer.close();
+
+
+
+//                ********* Clearing Temp_Request.xml *********
+        writer = Files.newBufferedWriter(Paths.get(getTemp_requestPath()));
+        writer.write("");
+        writer.flush();
+
+    }
+
+
+    public static void UpdatePayload() throws IOException, ParserConfigurationException, SAXException, TransformerException
+    {
+
+        //        ********** Reading Testdata from Excel ************
+
+        FileInputStream fis=new FileInputStream(new File(getTestData()));
+        XSSFWorkbook wb = new XSSFWorkbook(fis);
+        XSSFSheet sheet = wb.getSheet("AirInventoryService");
+        XSSFRow InputRow=sheet.getRow(6); //Taking scenario create booking for 1 pax
+
+        String filepath1;
+        filepath1=getRequestDirectory()+"AirInventoryService\\HA_Inventory_request_without_specifying_a_Date.xml";
+
+
+        XMLParser.SetTagtextatIndex("air1:FlightNumber",InputRow.getCell(2).getStringCellValue(),filepath1,0);
+        XMLParser.updateAttributeValue("air1:OriginLocation","LocationCode",InputRow.getCell(3).getStringCellValue(),getTemp_requestPath());
+        XMLParser.updateAttributeValue("air1:DestinationLocation","LocationCode",InputRow.getCell(4).getStringCellValue(),getTemp_requestPath());
+
+
+        wb.close();
+
+    }
+
+
+
+
+}
