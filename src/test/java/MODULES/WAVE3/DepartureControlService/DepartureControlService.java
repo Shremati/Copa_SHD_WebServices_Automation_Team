2619e910d9e7050f0a0ca6393da44eba1acf5cb7
Sauvik Sarkar <sauvik.sarkar@dxc.com>
2023-09-15 11:17:00 +0530

    DisplayLoyaltyAccountService,ManageSessions,etc done

--- src/test/java/MODULES/WAVE3/DepartureControlService/DepartureControlService.java
+++ src/test/java/MODULES/WAVE3/DepartureControlService/DepartureControlService.java
@@ -8,6 +8,7 @@ import static GENERICS.Utils.createFolders;
 import static GENERICS.Utils.failTest;
 import static frameworkconstants.FrameworkConstants.getResponseDirectory;
 
+//16 Scenarios
 public class DepartureControlService {
 
     DepartureControlService()
@@ -17,197 +18,197 @@ public class DepartureControlService {
 
     @Description("DPS_01 - Assign aircraft")
     @Test
-    public void Scenario1() {
+    public void DPS_01() {
 
         try {
             assign_aircraft.Execute();
 
         } catch (Exception e) {
             failTest(e);
-            System.out.println("SCENARIO 1 failed due to :" + e);
+            System.out.println("DPS_01 failed due to :" + e);
         }
     }
 
     @Description("DPS_07 - Assign aircraft with generic and specific seat re accommodation")
     @Test
-    public void Scenario2() {
+    public void DPS_07() {
 
         try {
             assign_aircraft_with_generic_specific_seat_reaccommodation.Execute();
 
         } catch (Exception e) {
             failTest(e);
-            System.out.println("SCENARIO 2 failed due to :" + e);
+            System.out.println("DPS_07 failed due to :" + e);
         }
     }
 
 
     @Description("DPS_10 - Start Checkin")
     @Test
-    public void Scenario3() {
+    public void DPS_10() {
 
         try {
             start_checkin.Execute();
 
         } catch (Exception e) {
             failTest(e);
-            System.out.println("SCENARIO 3 failed due to :" + e);
+            System.out.println("DPS_10 failed due to :" + e);
         }
     }
 
     @Description("DPS_13 - Block more than one seat")
     @Test
-    public void Scenario4() {
+    public void DPS_13() {
 
         try {
             block_more_than_one_seat.Execute(); //Give proper seat details in data sheet
 
         } catch (Exception e) {
             failTest(e);
-            System.out.println("SCENARIO 4 failed due to :" + e);
+            System.out.println("DPS_13 failed due to :" + e);
         }
     }
 
     @Description("DPS_14 - Error on block seat - Invalid seat number")
     @Test
-    public void Scenario5() {
+    public void DPS_14() {
 
         try {
             error_on_block_seat_invalid_seat_number.Execute();
 
         } catch (Exception e) {
             failTest(e);
-            System.out.println("SCENARIO 5 failed due to :" + e);
+            System.out.println("DPS_14 failed due to :" + e);
         }
     }
 
     @Description("DPS_16 - Unblock more than one seat")
     @Test
-    public void Scenario6() {
+    public void DPS_16() {
 
         try {
             unblock_more_than_one_seat.Execute(); //Give proper seat details in data sheet
 
         } catch (Exception e) {
             failTest(e);
-            System.out.println("SCENARIO 6 failed due to :" + e);
+            System.out.println("DPS_16 failed due to :" + e);
         }
     }
 
     @Description("DPS_18 - Define inoperative seat")
     @Test
-    public void Scenario7() {
+    public void DPS_18() {
 
         try {
             define_inoperative_seat.Execute(); //Give proper seat details in data sheet
 
         } catch (Exception e) {
             failTest(e);
-            System.out.println("SCENARIO 7 failed due to :" + e);
+            System.out.println("DPS_18 failed due to :" + e);
         }
     }
 
     @Description("DPS_20 - Clear inoperative seat")
     @Test
-    public void Scenario8() {
+    public void DPS_20() {
 
         try {
             clear_inoperative_seat.Execute();//Give proper seat details in data sheet
 
         } catch (Exception e) {
             failTest(e);
-            System.out.println("SCENARIO 8 failed due to :" + e);
+            System.out.println("DPS_20 failed due to :" + e);
         }
     }
 
     @Description("DPS_22 - Cancel Misconnect")
     @Test
-    public void Scenario9() {
+    public void DPS_22() {
 
         try {
             cancel_misconnect.Execute();  //Give proper flight details in data sheet
 
         } catch (Exception e) {
             failTest(e);
-            System.out.println("SCENARIO 9 failed due to :" + e);
+            System.out.println("DPS_22 failed due to :" + e);
         }
     }
 
     @Description("DPS_25 - Reinstate connecting passengers")
     @Test
-    public void Scenario10() {
+    public void DPS_25() {
 
         try {
             reinstate_connecting_passengers.Execute();
 
         } catch (Exception e) {
             failTest(e);
-            System.out.println("SCENARIO 10 failed due to :" + e);
+            System.out.println("DPS_25 failed due to :" + e);
         }
     }
 
     @Description("DPS_28 - Restrict checkin")
     @Test
-    public void Scenario11() {
+    public void DPS_28() {
 
         try {
             restrict_checkin.Execute();
 
         } catch (Exception e) {
             failTest(e);
-            System.out.println("SCENARIO 11 failed due to :" + e);
+            System.out.println("DPS_28 failed due to :" + e);
         }
     }
 
     @Description("DPS_32 - Display board point messages")
     @Test
-    public void Scenario12() {
+    public void DPS_32() {
 
         try {
             display_board_point_messages.Execute();
 
         } catch (Exception e) {
             failTest(e);
-            System.out.println("SCENARIO 13 failed due to :" + e);
+            System.out.println("DPS_32 failed due to :" + e);
         }
     }
 
     @Description("DPS_36 - Add/Update/Delete board point messages")
     @Test
-    public void Scenario13() {
+    public void DPS_36() {
 
         try {
             add_Update_Delete_board_point_messages.Execute(); //Before running test case , open the request in XML request directory and change the REMARKS which you want to add/delete
 
         } catch (Exception e) {
             failTest(e);
-            System.out.println("SCENARIO 14 failed due to :" + e);
+            System.out.println("DPS_36 failed due to :" + e);
         }
     }
 
     @Description("DPS_38 - Restrict boarding")
     @Test
-    public void Scenario14() {
+    public void DPS_38() {
 
         try {
             restrict_boarding.Execute();
 
         } catch (Exception e) {
             failTest(e);
-            System.out.println("SCENARIO 15 failed due to :" + e);
+            System.out.println("DPS_38 failed due to :" + e);
         }
     }
 
     @Description("DPS_39 - Remove boarding restriction")
     @Test
-    public void Scenario15() {
+    public void DPS_39() {
 
         try {
             remove_boarding_restriction.Execute();
 
         } catch (Exception e) {
             failTest(e);
-            System.out.println("SCENARIO 16 failed due to :" + e);
+            System.out.println("DPS_39 failed due to :" + e);
         }
     }
 }
