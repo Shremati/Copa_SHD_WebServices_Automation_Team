f0e59b760e84d7cdd15356d6003304e2f833be52
Sauvik Sarkar <sauvik.sarkar@dxc.com>
2023-09-18 19:23:30 +0530

    Modifications by Harshitha

--- src/test/java/MODULES/WAVE3/ModifyBookingService/ModifyBookingService.java
+++ src/test/java/MODULES/WAVE3/ModifyBookingService/ModifyBookingService.java
@@ -16,68 +16,68 @@ public class ModifyBookingService {
 
     @Description("MBS_01 - Cancel Booking")
     @Test
-    public void Scenario1() {
+    public void MBS_01() {
 
         try {
             cancel_booking.Execute(); //ModificationType="1"  --> Cancel Booking
 
         } catch (Exception e) {
             failTest(e);
-            System.out.println("SCENARIO 1 failed due to :" + e);
+            System.out.println("MBS_01 failed due to :" + e);
         }
     }
 
     @Description("MBS_04 - Itinerary Changes")
     @Test
-    public void Scenario2() {
+    public void MBS_04() {
 
         try {
             Itinerary_changes.Execute(); //ModificationType="5" --> Itenary Changes
 
         } catch (Exception e) {
             failTest(e);
-            System.out.println("SCENARIO 1 failed due to :" + e);
+            System.out.println("MBS_04 failed due to :" + e);
         }
     }
 
     @Description("MBS_07 - Other Changes")
     @Test
-    public void Scenario3() {
+    public void MBS_07() {
 
         try {
             other_changes.Execute(); //ModificationType="5" -->Other Changes
 
         } catch (Exception e) {
             failTest(e);
-            System.out.println("SCENARIO 3 failed due to :" + e);
+            System.out.println("MBS_07 failed due to :" + e);
         }
     }
 
 
     @Description("MBS_08 - Divide PNR")
     @Test
-    public void Scenario4() {
+    public void MBS_08() {
 
         try {
             divide_pnr.Execute(); //ModificationType="6" --> Divide PNR ( RPH=2 and RPH=4 , i.e. 2 pax getting divided)
 
         } catch (Exception e) {
             failTest(e);
-            System.out.println("SCENARIO 4 failed due to. :" + e);
+            System.out.println("MBS_08 failed due to. :" + e);
         }
 
     }
 
     @Description("MBS_10 - Reduce PNR")
     @Test
-    public void Scenario5() {
+    public void MBS_10() {
 
         try {
             reduce_pnr.Execute(); //ModificationType="8" -->Reduce PNR
 
         } catch (Exception e) {
             failTest(e);
-            System.out.println("SCENARIO 1 failed due to :" + e);
+            System.out.println("MBS_10 failed due to :" + e);
         }
 
     }
